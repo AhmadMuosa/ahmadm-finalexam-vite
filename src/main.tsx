2f14256f60814cdaf71e466fd9fa40ef1b37b8c1
Ahmad Muosa <muosa@sheridancollege.ca>
2026-08-13 12:07:56 -0400

    commented

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 
+//createRoot Function
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
