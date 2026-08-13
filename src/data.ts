c0184bee73732aeb22a8be53c8ed65f382730ce9
Ahmad Muosa <muosa@sheridancollege.ca>
2026-08-13 11:54:13 -0400

    Created data.ts file

---
+++ src/data.ts
@@ -0,0 +1,6 @@
+export const EXAM_INFO = {
+  title: "Final Exam",
+  studentName: "Your Name",
+  college: "Sheridan College",
+  courseName: "SYST24444 Mobile Web-Based Application Development",
+}
